9c68254acba501a799d156f1fc5844e49ed5c827
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-04 15:44:17 -0700

    added Pseudo code

--- assets/script.js
+++ assets/script.js
@@ -8,16 +8,46 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+
+  // Pseudo:
+// make multiple timeblocks
+// rename timeblocks accordingly with standard work hours
+// global event listener to make sure clicking on the save button
+// use event target, to traverse the DOM
+// the save button is to save input into the timeblock into local storage
+//   
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+
+       // Pseudo:
+// declare current time for callback as a variable
+// create an array and place DOM nodes in the array / DOM node: grabs an element on the page.
+// Use loop to iterate through array to compare to current date/time using dayjs()
+// assign past, present, and future classes
+// 
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+  // Pseudo:
+// Grab information from local storage, in correspondence to the time of the day/ locate the appropriate timeblock and set value of input/textarea to the corresponding data to that timeblock
+// 
+
   // TODO: Add code to display the current date in the header of the page.
+
+  // Pseudo:
+// make a variable that is for the current day with dayjs
+// set all dates in currentDate to variable of dayjs()
+// 1 object per time slot / or 1 that encompasses all of them / 
+
 });
