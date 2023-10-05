f610e30f933450446b5d3bb4eb6bd89297c1cfed
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-05 08:32:17 -0700

    added eventListner

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ function currentTime(){
 currentTime();
 
 var mainContainer = $("#container");
-var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var workHours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 var currentHour = dayjs().hour();
 
 $(function () {
@@ -40,31 +40,60 @@ $(function () {
     var textArea = $("<textarea>");
     textArea.addClass("col-8 col-md-10 description");
     textArea.attr("id", `textArea-${i}`);
-  
-
+    
     var saveBtn = $("<button>");
     saveBtn.addClass("btn saveBtn col-2 col-md-1");
     saveBtn.attr("aria-label", "save");
+    saveBtn.attr("id", `saveText-${i}`);
 
     var btnIcon = $("<i>");
     btnIcon.addClass("fas fa-save");
     btnIcon.attr("aria-hidden", "true");
+    
 
     saveBtn.append(btnIcon);
     parentDiv.append(hourDiv, textArea, saveBtn);
     mainContainer.append(parentDiv);
 
-    // var past = document.querySelector("#past");
-    // past.addEventListener("click", function(event){
-    //   var element = event.target;
-    //   if (element.matches("#past")) {
-    //   saveBtn.prop("disabled", true);
-    //   textArea.prop("disabled", true);
-    //   } })
+
+    
+
+
+  var saveText = document.querySelector(`saveText-${i}`);
+
+  saveText.addEventListener("click", function(event){
+    event.preventDefault();
+  //   var daySave = "textArea.textContent";
+  localStorage.setItem("dayHour", daySave);
+  })
+  
     
   }
   
+  
+    
 
+    //var disableBtn = document.getElementsByClassName("row time-block past");
+
+
+
+
+
+    // function hourSaved(){
+    //   var saveText = localStorage.getitem("#`textArea-${i}`");
+    //   saveText.textContent = savedText;
+    // }
+
+    // saveBtn.addEventListener("click", function(event) {
+    
+    // localStorage.setItem("#`textArea-${i}`", savedText);
+    // hourSaved();})
+
+
+  //   var element = event.target;
+  // if (element.matches("#past")) {
+  // saveBtn.prop("disabled", true);
+  // textArea.prop("disabled", true);
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -72,7 +101,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
+  
 
   // Pseudo:
 // make multiple timeblocks
