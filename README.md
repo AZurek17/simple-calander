f2dac75c5d3a6b689e392c02205a1b42573fa3bf
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-05 23:07:08 -0700

    corrected localstorage code,  cleaned up codes, README, and added screenshot

--- README.md
+++ README.md
@@ -5,34 +5,64 @@
 ![simple-calander](Screenshot.png)
 
 ### Technoloy used:
-* JavaScript - If...Else - https://www.w3schools.com/jsref/jsref_if.asp
+* JavaScript - If... Statement - https://www.w3schools.com/jsref/jsref_if.asp
 * JavaScript - Logical Operators - https://www.w3schools.com/java/java_operators.asp
 * JavaScript - For Loop - https://www.w3schools.com/java/java_for_loop.asp
-* JavaScript - Math.random() - https://www.w3schools.com/jsref/jsref_random.asp
+* jquery - https://jquery.com/
+* Day.js - https://day.js.org/
 
 ## Description
 
-A simple calander which allows a person to save comments for each hour during typical work hours.
+A simple calander which allows a person to save comments for each hour during typical work hours.  Start code was provided. See credits for URL link. 
 
 ## Usage
-When you go to the website, you will see the the current day. 
+When you go to the website, you will see the the current day. this was accomplished by using Day.js.
 
-as you scroll down, you will see hourly time blocks.   
+As you scroll down, you will see hourly time blocks, which you can input text into the timeblock and save.  this was accomplished using jQuery.   
 
 Below are a few examples of code:
  -------------------------------
 
-### 
+### Day.js
 
-###
 
-###
+    function currentTime(){
+    var today = dayjs();
+    $("#currentDay").text(today.format("dddd - MMMM DD, YYYY"));
+    }
+    currentTime();
 
-###
+
+ -------------------------------
+
+### Jquery & EventListner
+
+
+    $(".saveBtn").on("click", function (){
+      var hourSaved = $(this).parent().attr("id");
+      var userInput = $(this).siblings(".description").val();
+      console.log(localStorage);
+      localStorage.setItem(hourSaved, userInput);
+    });
+
+
+ -------------------------------
+
+### For Loop and IF statement
+
+
+    for (var i = 0; i < workHours.length; i++){
+        var hours = `${workHours[i]} a.m.`;
+        if(workHours[i] > 11){
+        hours = `${workHours[i]} p.m.`;
+        if(workHours[i] > 12)  hours = `${workHours[i] - 12} p.m.`;
+        };
+
 
 
 ## Credits
 * Stater code provided: https://github.com/coding-boot-camp/crispy-octo-meme
+* AskBCS Learning Assistant: Jchance
 * Tutoring Session: Corrado Alfano
 * StudyGroup
 
--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,35 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn"t run until the browser has finished rendering all the elements
-// in the html.
-
-function currentTime(){
-   var today = dayjs();
-  $("#currentDay").text(today.format("dddd - MMMM DD, YYYY"));
-}
-currentTime();
-
-
-var mainContainer = $("#container");
-var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-var currentHour = dayjs().hour();
-
 
 $(function () {
 
+ function currentTime(){
+   var today = dayjs();
+   $("#currentDay").text(today.format("dddd - MMMM DD, YYYY"));
+ }
+ currentTime();
+ 
+ var mainContainer = $("#container");
+ var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+ var currentHour = dayjs().hour();
   for (var i = 0; i < workHours.length; i++) {
-
+// converting military time to AM, PM
     var hours = `${workHours[i]} a.m.`;
     if(workHours[i] > 11){
       hours = `${workHours[i]} p.m.`;
       if(workHours[i] > 12)  hours = `${workHours[i] - 12} p.m.`;
+// getting saved inputs to display to user
+    var savedTask = localStorage.getItem(`hour-${workHours[i]}`);
+      if (savedTask) {
+      textArea.val(savedTask);
+      }
     }
-
+// adding div classes and id's for HMTL
     var parentDiv = $("<div>");
     parentDiv.attr("id", `hour-${workHours[i]}`);
     parentDiv.addClass("row time-block");
+    
     if(currentHour < workHours[i])parentDiv.addClass("future");
     if(currentHour === workHours[i])parentDiv.addClass("present");
     if(currentHour > workHours[i])parentDiv.addClass("past");
-    
-    
-    
-    //if(currentHour > workHours[i]){parentDiv.attr("id", `hour-${workHours[i]}, past`);}
-
-    //if (currentHour > workHours[i])
     
     var hourDiv = $("<div>");
     hourDiv.addClass("col-2 col-md-1 hour text-center py-3");
@@ -48,49 +42,24 @@ $(function () {
     var saveBtn = $("<button>");
     saveBtn.addClass("btn saveBtn col-2 col-md-1");
     saveBtn.attr("aria-label", "save");
-
-    saveBtn.attr("id", `saveText-${i}`);//////////////////////////////
+    saveBtn.attr("id", `saveText-${i}`);
 
     var btnIcon = $("<i>");
     btnIcon.addClass("fas fa-save");
     btnIcon.attr("aria-hidden", "true");
-    
 
     saveBtn.append(btnIcon);
     parentDiv.append(hourDiv, textArea, saveBtn);
     mainContainer.append(parentDiv);
 
-    function showOutput() {
-      var userOutput = localStorage.getItem("id", `textArea-${i}`);
-    }
-    
-
+// EventListener for save button
     $(".saveBtn").on("click", function (){
       var hourSaved = $(this).parent().attr("id");
-      var userInput =$(this).siblings(".description").val();
+      var userInput = $(this).siblings(".description").val();
       console.log(localStorage);
-      //localStorage.setItem(hourSaved, userInput);
-      //localStorage.getItem(hourSaved)
-      showOutput()
-      
-      //console.log(userOutput);
+      localStorage.setItem(hourSaved, userInput);
     });
-   
-
-
-    // userInput = ("id", `textArea-${i}`).text();
-    // localStorage.setItem(hourData, JSON.stringify(userInput));
-    // localStorage.getItemItem(hourData);
-  
-    //var userOutput = JSON.parse(localStorage.getItem(userInput))
-    //console.log(userOutput);
-    //var userInput = (localStorage.getItem("id", `textArea-${i}`).innerHTML);
-    //console.log(userInput);
-  
-    
-    
   }
-
 });
 
 
